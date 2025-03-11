1427362365a63c72f6f78d3758f5ce1026e4f337
Benito Flores <118690844+Benito-Flores@users.noreply.github.com>
2025-03-10 20:09:43 -0700

    Add files via upload
    added addition function and equal function with addition to calculator

--- script.js
+++ script.js
@@ -28,69 +28,96 @@ const clearBtn = document.querySelector(".clear-btn");
 
 // global variables
 let total = 0;
-let boxContent = [];
+let boxContent = "";
+let operatorType = "";
 
 // clears text box, array, and sets global total to zero
 clearBtn.addEventListener("click", () => {
   txtBox.innerHTML = "";
-  boxContent = [];
+  boxContent = "";
   total = 0;
 });
 
-// adds number to text box
+// operand buttons; adds number to text box
 zeroBtn.addEventListener("click", () => {
   if (boxContent.length > 0) {
-    boxContent.push(0); // stores new index to array
-    txtBox.textContent = boxContent.join("");
+    boxContent += 0;
+    txtBox.textContent = boxContent;
   };
 });
 
 oneBtn.addEventListener("click", () => {
-  boxContent.push(1); // stores new index to array
-  txtBox.textContent = boxContent.join("");
+  boxContent += 1;
+  txtBox.textContent = boxContent;
 });
 
 twoBtn.addEventListener("click", () => {
-  boxContent.push(2); // stores new index to array
-  txtBox.textContent = boxContent.join("");
+  boxContent += 2;
+  txtBox.textContent = boxContent;
 });
 
 threeBtn.addEventListener("click", () => {
-  boxContent.push(3); // stores new index to array
-  txtBox.textContent = boxContent.join("");
+  boxContent += 3;
+  txtBox.textContent = boxContent;
 });
 
 fourBtn.addEventListener("click", () => {
-  boxContent.push(4); // stores new index to array
-  txtBox.textContent = boxContent.join("");
+  boxContent += 4;
+  txtBox.textContent = boxContent;
 });
 
 fiveBtn.addEventListener("click", () => {
-  boxContent.push(5); // stores new index to array
-  txtBox.textContent = boxContent.join("");
+  boxContent += 5;
+  txtBox.textContent = boxContent;
 });
 
 sixBtn.addEventListener("click", () => {
-  boxContent.push(6); // stores new index to array
-  txtBox.textContent = boxContent.join("");
+  boxContent += 6;
+  txtBox.textContent = boxContent;
 });
 
 sevenBtn.addEventListener("click", () => {
-  boxContent.push(7); // stores new index to array
-  txtBox.textContent = boxContent.join("");
+  boxContent += 7;
+  txtBox.textContent = boxContent;
 });
 
 eightBtn.addEventListener("click", () => {
-  boxContent.push(8); // stores new index to array
-  txtBox.textContent = boxContent.join("");
+  boxContent += 8;
+  txtBox.textContent = boxContent;
 });
 
 nineBtn.addEventListener("click", () => {
-  boxContent.push(9); // stores new index to array
-  txtBox.textContent = boxContent.join("");
+  boxContent += 9;
+  txtBox.textContent = boxContent;
+});
+
+// operator buttons
+
+equalBtn.addEventListener("click", () => {
+  if (operatorType === "add") {
+    total = total + +boxContent;
+    txtBox.textContent = total;
+    boxContent = "";
+  } else if (operatorType === "minus") {
+
+  } else if (operatorType === "multiply") {
+    
+  } else if (operatorType === "divide") {
+
+  };
+});
+
+// addition operator event listener
+addBtn.addEventListener("click", () => {
+  operatorType = "add"; // operator type for qual btn event listener
+  total += +boxContent;
+  boxContent = "";
+  txtBox.textContent = total;
 });
 
 divideBtn.addEventListener("click", () => {
-  console.log(typeof boxContent[0]);
+  console.log(`total is ${total}`);
+  console.log(`total type is a ${typeof total}`);
+  console.log(`boxContent is ${boxContent}`);
+  console.log(`boxContent type is a ${typeof boxContent}`);
 });
-
